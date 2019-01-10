7a6d1b2051d0c06f50a1b5c929cf86e308a4e56d
randomblitz9 <randomblitz9@gmail.com>
2019-01-10 15:50:24 +0800

    refactor

--- basicCrawlingFindallTagsTutorial.py
+++ basicCrawlingFindallTagsTutorial.py
@@ -2,8 +2,18 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
 bsObj = BeautifulSoup(html)
-nameList = bsObj.findAll("span", {"class":"green","class":"red"})
-for name in nameList:
-    print(name.get_text())
+#nameList = bsObj.findAll("span", {"class":"green","class":"red"})
+#for name in nameList:
+#    print(name.get_text())
     
     
+nameList = bsObj.findAll(text="I think,")
+##print(len(nameList))    
+
+
+allText = bsObj.findAll(id="text")
+#search for pa specific attriute
+
+allText = bsObj.findAll("",{"id":"text"});
+#same as above
+print(allText[0].get_text())
